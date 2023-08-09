d00af3babc87e1c1d08b57adb2a1fce9445e4156
Lorenzo Gomez <lologg03@gmail.com>
2023-08-09 14:54:28 -0300

    misEstados

--- misEventos.php
+++ misEventos.php
@@ -34,18 +34,24 @@ $id = $_SESSION['id'];
 // Conectarse a la base de datos (suponiendo que tengas la función connect() definida)
 $conn = connect();
 
+$darMensaje = true;
+
 if (isset($_POST['filtrar'])){
     if(!empty($_POST['fecha-hora'])){
         $fecha_hora = $_POST['fecha-hora'];
+        $filtro = "DATE(fecha_inicio) = '".$fecha_hora."'";
+        $darMensaje = false;
+        $limite = "";
+    } else {
+        $filtro = "1";
+        $limite = "limit 20";
     }
-    $filtro = "DATE(fecha_inicio) = '".$fecha_hora."'";
-    $darMensaje = false;
 }else{
-    $filtro = "DATE(fecha_inicio) = '".date("Y-m-d")."'";
-    $darMensaje = true;
+    $filtro = "1";
+    $limite = "limit 20";
 }
 
-$sql = "SELECT * from eventos WHERE ".$filtro." AND id in (SELECT evento_id FROM eventoUsuarios where usuario_id = '".$id."') ORDER BY fecha_inicio";
+$sql = "SELECT * from eventos WHERE ".$filtro." AND id in (SELECT evento_id FROM eventoUsuarios where usuario_id = '".$id."') ORDER BY fecha_inicio DESC ".$limite;
 $result = mysqli_query($conn, $sql);
 
 $actualizarNotificacion="UPDATE notificaciones SET visto = true WHERE usuario_id = '".$id."'";
@@ -62,7 +68,7 @@ include('templates/head.php')
     <div class="container">
          <form class="d-inline"  method="post" style="vertical-align: center" action="">
             <label class=" d-inline" for="fecha-hora">Fecha:</label>
-            <input class="form-date d-inline ms-2 pt-1 pb-1" type="date" id="fecha-hora" name="fecha-hora" required >
+            <input class="form-date d-inline ms-2 pt-1 pb-1" type="date" id="fecha-hora" name="fecha-hora">
             <button type="submit" class="btn-general ms-2 px-3  d-inline" name="filtrar">
                 <i class="bi bi-funnel fs-6 me-1"></i> Filtrar
             </button>
@@ -72,7 +78,7 @@ include('templates/head.php')
     if (mysqli_num_rows($result) > 0){
         if ($darMensaje == true){
 ?>
-            <h4 class="alert alert-info p-3 text-center">Eventos del día </h4>
+            <h4 class="alert alert-info p-3 text-center">Ultimos eventos</h4>
             <?php
         }
 ?>
@@ -82,6 +88,7 @@ include('templates/head.php')
             <th scope="col">Inicio</th>
             <th scope="col">Fin</th>
             <th scope="col">Lugar</th>
+            <th scope="col">Estado</th>
             </tr>
             </thead>
                 <?php while ($row = mysqli_fetch_assoc($result)){ ?>
@@ -89,6 +96,17 @@ include('templates/head.php')
                  <td> <?php echo  $row['fecha_inicio'] ?> </td>
                  <td> <?php echo  $row['fecha_fin'] ?> </td>
                  <td> <?php echo  $row['lugar']  ?></td>
+                <?php
+                if ($row['fecha_fin'] < date("Y-m-d H:i:s")){
+                ?>
+                    <td>Finalizado</td>
+                <?php
+                } else {
+                ?>
+                    <td>Pendiente</td>
+                <?php
+                }
+                ?>
             </tr>
                   
                 <?php } ?>
