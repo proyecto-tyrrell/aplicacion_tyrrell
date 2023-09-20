6de28f130e64bbccbf6821483a1bf94edd533940
Lorenzo Gomez <lologg03@gmail.com>
2023-09-20 11:09:58 -0300

    agregar eventos multiples funcional

--- agregarEvento.php
+++ agregarEvento.php
@@ -40,6 +40,11 @@ $proyectos = mysqli_query($conn, $sqlProyectos);
 include('templates/head.php')
 ?>
 
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+</head>
+
 <?php include('templates/header.php')?>
 <?php include('templates/nav.php')?>
 
@@ -60,13 +65,23 @@ include('templates/head.php')
                 ?>
                 </select>
             </div>
+            <div>
+                <label for="dates">Fecha:</label>
+                <input type="text" class="form-control" id="dates" name="fechas" placeholder="Selecciona fechas" readonly>
+            </div>
+            <script>
+                flatpickr('#dates', {
+                    mode: 'multiple',
+                    dateFormat: 'Y-m-d', // Formato de fecha deseado
+                });
+            </script>
             <div class="m-4 mx-0">
-                <label for="fecha-inicio">Fecha y hora de inicio:</label>
-                <input class="form-control" type="datetime-local" id="fecha-inicio" name="fecha-inicio" required>
+                <label for="fecha-inicio">Hora de inicio:</label>
+                <input class="form-control" type="time" id="fecha-inicio" name="fecha-inicio" required>
             </div>
             <div class="m-4 mx-0">
-                <label for="fecha-fin">Fecha y hora de fin:</label>
-                <input class="form-control" type="datetime-local" id="fecha-fin" name="fecha-fin" required>
+                <label for="fecha-fin">Hora de fin:</label>
+                <input class="form-control" type="time" id="fecha-fin" name="fecha-fin" required>
             </div>
             <div>
                 <label for="lugar">Lugar:</label>
@@ -79,12 +94,13 @@ include('templates/head.php')
 
         <?php
     if (isset($_POST['siguiente'])){
-        if ((!empty($_POST['proyecto'])) && (!empty($_POST['fecha-inicio'])) && (!empty($_POST['fecha-fin'])) && (!empty($_POST['lugar']))){
+        if ((!empty($_POST['proyecto'])) && (!empty($_POST['fecha-inicio'])) && (!empty($_POST['fecha-fin'])) && (!empty($_POST['lugar'])) && (!empty($_POST['fechas']))){
             if ($_POST['fecha-inicio'] < $_POST['fecha-fin']){
                 $proyecto_id = $_POST['proyecto'];
                 $fecha_inicio = $_POST['fecha-inicio'];
                 $fecha_fin = $_POST['fecha-fin'];
                 $lugar = $_POST['lugar'];
+                $fechas_seleccionadas = $_POST['fechas'];
 
                 // vehiculos
                 $sqlVehiculos = "SELECT * FROM vehiculos WHERE id NOT IN (
@@ -140,6 +156,7 @@ include('templates/head.php')
                     <input name="fecha-inicio" value="<?php echo $fecha_inicio; ?>" type="hidden">
                     <input name="fecha-fin" value="<?php echo $fecha_fin; ?>" type="hidden">
                     <input name="lugar" value="<?php echo $lugar; ?>" type="hidden">
+                    <input name="fechas" value='<?php echo $fechas_seleccionadas ?>' type="hidden">
                     <button type="submit" class="btn-general px-4" name="cargar">Cargar</button>
                 </div>
 
@@ -159,36 +176,43 @@ include('templates/head.php')
 
     if ((isset($_POST['cargar'])) && (!empty($_POST['usuarios']))){
         $proyecto_id = $_POST['proyecto'];
-        $fecha_inicio = $_POST['fecha-inicio'];
-        $fecha_fin = $_POST['fecha-fin'];
+        $hora_inicio = $_POST['fecha-inicio'];
+        $hora_fin = $_POST['fecha-fin'];
         $lugar = $_POST['lugar'];
-        //crear evento
-        $sql = "INSERT INTO eventos (usuario_id, proyecto_id, fecha_inicio, fecha_fin, lugar) VALUES ('".$_SESSION['id']."', '".$proyecto_id."', '".$fecha_inicio."', '".$fecha_fin."', '".$lugar."')";
-        mysqli_query($conn, $sql);
-        $evento_id = mysqli_insert_id($conn);//obtener id del evento recien creado
+        $fechas_seleccionadas = explode(',', $_POST['fechas']);
+        
+        foreach ($fechas_seleccionadas as $fecha){
 
-        if (!empty($_POST['vehiculos'])){
-            $vehiculosSeleccionados = $_POST['vehiculos'];
+            $fecha_inicio = $fecha . " " . $hora_inicio;
+            $fecha_fin = $fecha . " " . $hora_fin;
+            
+            //crear evento
+            $sql = "INSERT INTO eventos (usuario_id, proyecto_id, fecha_inicio, fecha_fin, lugar) VALUES ('".$_SESSION['id']."', '".$proyecto_id."', '".$fecha_inicio."', '".$fecha_fin."', '".$lugar."')";
+            mysqli_query($conn, $sql);
+            $evento_id = mysqli_insert_id($conn);//obtener id del evento recien creado
 
-            // Asignar vehículos al evento
-            foreach ($vehiculosSeleccionados as $vehiculo_id) {
-            // echo $vehiculo_id;
-                $sqlAsignarVehiculo = "INSERT INTO eventoVehiculos (evento_id, vehiculo_id) VALUES ('$evento_id', '$vehiculo_id')";
-                mysqli_query($conn, $sqlAsignarVehiculo);
-            }
-        }            
+            if (!empty($_POST['vehiculos'])){
+                $vehiculosSeleccionados = $_POST['vehiculos'];
 
-        $usuariosSeleccionados = $_POST['usuarios'];
+                // Asignar vehículos al evento
+                foreach ($vehiculosSeleccionados as $vehiculo_id) {
+                // echo $vehiculo_id;
+                    $sqlAsignarVehiculo = "INSERT INTO eventoVehiculos (evento_id, vehiculo_id) VALUES ('$evento_id', '$vehiculo_id')";
+                    mysqli_query($conn, $sqlAsignarVehiculo);
+                }
+            }            
 
-        // Asignar usuarios al evento
-        foreach ($usuariosSeleccionados as $usuario_id) {
-            //echo $usuario_id;
-            $sqlAsignarUsuario = "INSERT INTO eventoUsuarios (evento_id, usuario_id) VALUES ('$evento_id', '$usuario_id')";
-            mysqli_query($conn, $sqlAsignarUsuario);
-            $sqlNotificacion = "INSERT INTO notificaciones (usuario_id, evento_id, visto) VALUES ('$usuario_id', '$evento_id', false)";
-            mysqli_query($conn, $sqlNotificacion);
-        }
-        ?>
+            $usuariosSeleccionados = $_POST['usuarios'];
+
+            // Asignar usuarios al evento
+            foreach ($usuariosSeleccionados as $usuario_id) {
+                //echo $usuario_id;
+                $sqlAsignarUsuario = "INSERT INTO eventoUsuarios (evento_id, usuario_id) VALUES ('$evento_id', '$usuario_id')";
+                mysqli_query($conn, $sqlAsignarUsuario);
+                $sqlNotificacion = "INSERT INTO notificaciones (usuario_id, evento_id, visto) VALUES ('$usuario_id', '$evento_id', false)";
+                mysqli_query($conn, $sqlNotificacion);
+            } 
+        }?>
         <p class="alert alert-success text-center" >Se ha cargado con éxito</p>
         <?php
     }      
